2dd57219ec003cd4e18158b8ae31cf5fd19ba4ba
Rishi Banerjee <rishibanerjee10@gmail.com>
2025-02-24 03:58:37 +0530

    Added new logic for deployment

--- backend/index.js
+++ backend/index.js
@@ -15,7 +15,7 @@ dotenv.config();
 const PORT = process.env.PORT || 3000;
 const __dirname = path.resolve();
 const app = express();
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(cors());
 app.use(express.json({ limit: "50mb" })); // Adjust size as needed
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
 
